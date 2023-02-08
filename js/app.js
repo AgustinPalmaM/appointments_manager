4a3048900f040800d057d55b575a7c929e543f2f
AgustinPalmaM <palma.agustin7@gmail.com>
2023-02-08 13:38:32 -0300

    Create class Apppointment to manage appointment data and create array with all of the appointments Create class UI to manage user interface, also create a method in UI class to manage and print alerts

--- js/app.js
+++ js/app.js
@@ -10,6 +10,41 @@ const symptomsInput = document.querySelector('#sintomas');
 const form = document.querySelector('#nueva-cita');
 const appointmentsContainer = document.querySelector('#citas');
 
+class UI {
+  printAlert( message, typeAlert ) {
+    const mainContainer = document.querySelector('#contenido');
+    
+    while(mainContainer.firstElementChild.classList.contains('alert')){
+      mainContainer.firstElementChild.remove();
+    }
+    const divMessage = document.createElement('DIV');
+    divMessage.classList.add('text-center', 'alert', 'd-block', 'col-12');
+    divMessage.textContent = message;
+
+    if( typeAlert === 'error' ) {
+      divMessage.classList.add('alert-danger')
+    } else {
+      divMessage.classList.add('alert-success')
+    }
+
+    // add alert to the DOM
+    mainContainer.insertBefore(divMessage, document.querySelector('.agregar-cita'))
+
+    setTimeout(() => {
+      divMessage.remove();
+    }, 3000 )
+  }
+}
+
+class Appointment {
+  constructor() {
+    this.appointments = [];
+  }
+}
+
+const ui = new UI();
+const manageAppointments = new Appointment();
+
 // main object with appointment data
 const appointmentObject = {
   pet: '',
@@ -29,6 +64,8 @@ function eventListeners() {
   dateInput.addEventListener( 'change', appointmentData );
   hourInput.addEventListener( 'change', appointmentData );
   symptomsInput.addEventListener( 'change', appointmentData );
+
+  form.addEventListener( 'submit', newAppointment );
 }
 
 // add info to appointment object
@@ -37,3 +74,16 @@ function appointmentData(e) {
   console.log(appointmentObject);
 }
 
+// validate and fill new appointment to appointment class
+function newAppointment(e) {
+  e.preventDefault();
+
+  // extract dara from appointment object
+  const { pet, owner, phone, date, hour, symptoms } = appointmentObject;
+
+  if ( pet === '' || owner === '' || phone ==='' || date === '' || hour === '' || symptoms === '' ) {
+    ui.printAlert( 'All of the fields are required', 'error' );
+    return;
+  }
+
+}
